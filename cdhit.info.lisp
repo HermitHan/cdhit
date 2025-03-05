7047d259a9949eba753482b88f70d682e456d346
Kaiyuan Han <hermit_han@163.com>
2025-03-05 22:00:38 +0800

    Update cdhit.info.lisp

--- cdhit.info.lisp
+++ cdhit.info.lisp
@@ -2,5 +2,6 @@
 (:repo           "HermitHan/cdhit")
 (:author         "KaiyuanHan")
 (:version        "4.8.1")
+(:intro          "CD-HIT is a very widely used program for clustering and comparing protein or nucleotide sequences.")
 (:dependencies)
 (:functions      "cd-hit")
