2a39ddc83105c66e4f258eebc6fec343d3b9a297
Kaiyuan Han <hermit_han@163.com>
2025-03-08 01:05:36 +0800

    update taffish-org & registry to ghrc.io

--- cdhit.info.lisp
+++ cdhit.info.lisp
@@ -1,5 +1,5 @@
 (:name           "cdhit")
-(:repo           "HermitHan/cdhit")
+(:repo           "taffish-org/cdhit")
 (:author         "KaiyuanHan")
 (:version        "4.8.1")
 (:intro          "CD-HIT is a very widely used program for clustering and comparing protein or nucleotide sequences.")
